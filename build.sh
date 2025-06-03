11cc59441c002e2c224cd2d882fdac78412bd792
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-03 14:43:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y python3 python3-pip
+
+# (1) Update package lists
+apt-get update
+
+# (2) Install Python, pip, and CA certificates
+apt-get install -y python3 python3-pip ca-certificates
+
+# (3) Install snscrape via pip
 pip3 install snscrape
+
